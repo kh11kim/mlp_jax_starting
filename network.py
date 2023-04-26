778b26e28f4d4e1cdcb92fd459e8a236d6b66d39
kh11kim <kanghyun777@naver.com>
2023-04-26 18:03:49 +0900

    fixing

--- network.py
+++ network.py
@@ -4,6 +4,7 @@ import flax.linen as nn
 from typing import Callable, Sequence
 import orbax
 from flax.training import orbax_utils
+from flax.training.train_state import TrainState
 
 class LipLinear(nn.Module):
     """A linear layer with Lipschitz regularization."""
@@ -66,10 +67,66 @@ class MLP(nn.Module):
             x = self.out_actv_fn(x)
         return x
 
-def save(path, params):
+class Hyperparam:
+    """Class to store hyperparameters for a model.
+    
+    Usage:
+        hp = Hyperparam()
+        hp.key1 = value1
+        hp.key2 = value2
+        ...
+        if key in hp:
+            # do something
+        print(hp)
+        print(hp.to_str())
+    """
+    def __contains__(self, key):
+        return key in self.__dict__.keys()
+    
+    def __repr__(self):
+        return self.__dict__.__repr__()
+    
+    def as_str(self):
+        result = ""
+        for key, item in self.__dict__.items():
+            if key == "layers":
+                result += key + ":" + "_".join([str(l) for l in item])
+            else:
+                result += key + ":" + str(item)
+            result += ","
+        return result[:-1]
+    
+    def as_dict(self):
+        return self.__dict__
+    
+    @classmethod
+    def from_dict(cls, d:dict):
+        result = cls()
+        for k, v in d.items():
+            setattr(result, k, v)
+        return result
+
+def get_mlp(hp:Hyperparam):
+    skip_layer = 0 if "skip_layer" not in hp else hp.skip_layer
+    linear = nn.Dense if "linear" not in hp else getattr(nn, hp.linear)
+    actv_fn = nn.relu if "actv_fn" not in hp else getattr(nn, hp.actv_fn)
+    out_actv_fn = None if "out_actv_fn" not in hp else getattr(nn, hp.out_actv_fn)
+    return MLP(hp.dims, skip_layer, linear=linear, actv_fn=actv_fn, out_actv_fn=out_actv_fn)
+
+def get_mlp_by_path(path):
+    net_dict = load(path)
+    hp = Hyperparam.from_dict(net_dict['hp'])
+    net = get_mlp(hp)
+    net_bind = net.bind({"params":net_dict['params']})
+    return net_bind
+
+def save(path, state:TrainState, hp:Hyperparam, force=False):
+    params = state.params.unfreeze()
+    params['hp'] = hp.as_dict()
+    
     orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
     save_args = orbax_utils.save_args_from_target(params)
-    orbax_checkpointer.save(path, params, save_args=save_args)
+    orbax_checkpointer.save(path, params, save_args=save_args, force=force)
 
 def load(path):
     orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
